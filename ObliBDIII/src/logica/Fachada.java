5585572f7ea3668ed660244faf3049e0a99bb3d0
Candelaria <candelariafraquioom@gmail.com>
2024-10-28 20:06:23 -0300

    acondiciono fachada y comienzo con métodos de folios

--- ObliBDIII/src/logica/Fachada.java
+++ ObliBDIII/src/logica/Fachada.java
@@ -1,90 +1,106 @@
 
+
 package logica;
 
-import java.rmi.RemoteException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.List;
 
-import logica.excepciones.LogicaException;
-import logica.excepciones.PersistenciaException;
-import logica.valueObjects.VOFolio;
-import logica.valueObjects.VOFolioMaxRev;
-import logica.valueObjects.VORevision;
 import persistencia.daos.DAOFolios;
 
-/*import Ejercicio3.LogicaPersistencia.Excepciones.PersistenciaException;
-import Ejercicio3.LogicaPersistencia.ValueObjects.*;*/
+import Ejercicio3.LogicaPersistencia.Excepciones.PersistenciaException;
+import Ejercicio3.LogicaPersistencia.ValueObjects.*;
+import logica.valueObjects.VOFolio;
+import logica.valueObjects.VOFolioMaxRev;
 
-public class Fachada implements IFachada {
-	private DAOFolios Folios;
+public class Fachada {
+	
+	// acondicioné y arranqué con los de folio para adelantar que era solo invocar a los metodos del dao
+	// no creo que sean con catch, capaz throw, pero lo vemos mañana
+	
+	private DAOFolios daoFolios;
 
-	public void agregarFolio(VOFolio voF) throws PersistenciaException, RemoteException, LogicaException {
-		if (!Folios.member(voF.getCodigo())) {
-			Folio folio = new Folio(voF.getCodigo(), voF.getCaratula(), voF.getPaginas());
-			Folios.insert(folio);
-		} else
-			throw new LogicaException("Error");
+	public void agregarFolio(VOFolio voF) {
+		 try 
+		 {
+			 if(!daoFolios.member(voF.getCodigo()))
+			 {
+				 Folio folio = new Folio(voF.getCodigo(), voF.getCaratula(), voF.getPaginas());
+			 	 daoFolios.insert(folio);
+			 }	
+		 } catch(PersistenciaException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+		 }
 
 	}
-
-	public void agregarRevision(String codF, String desc)
-			throws PersistenciaException, RemoteException, LogicaException {
-		if (!Folios.member(codF)) {
-			throw new LogicaException("Error");
-		} else {
-			Folio fol = Folios.find(codF);
-			int numR = fol.cantidadRevisiones() + 1;
-			Revision rev = new Revision(numR, desc);
-			fol.addRevision(rev);
-		}
-
-	}
-
-	public void borrarFolioRevisiones(String codF) throws PersistenciaException, RemoteException, LogicaException {
-		if (Folios.member(codF)) {
-			Folio fol = Folios.find(codF);
-			fol.borrarRevisiones();
-			Folios.delete(codF);
-		} else
-			throw new LogicaException("No existe el folio");
-	}
-
-	public String darDescripcion(String codF, int numR) throws PersistenciaException, RemoteException, LogicaException {
-		String descripcion = null;
-		if (Folios.member(codF)) {
-			Folio fol = Folios.find(codF);
-			if (fol.tieneRevision(numR)) {
-				Revision rev = fol.obtenerRevision(numR);
-				descripcion = rev.getDescripcion();
-			} else {
-				throw new LogicaException("No existe la revision");
+	/*
+	public void agregarRevision(String codF, String desc) {
+		try {
+			if (ClaseAccesoBD.ExisteFolio(con, codF)) {
+				int numero = ClaseAccesoBD.calcularUltimaRevision(con) + 1;
+				VORevision r = new VORevision(numero, codF, desc);
+				ClaseAccesoBD.AgregarRevision(con, r);
 			}
-		} else
-			throw new LogicaException("No existe el folio");
+		} catch (PersistenciaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	*/
+	public void borrarFolioRevisiones(String codF) {
+		try {
+			if (daoFolios.member(codF)) {
+				// ClaseAccesoBD.BorrarFolioRevisiones(con, codF);  Acá me genera dudas lo que dice de que accede interno
+				daoFolios.delete(codF);
+			}
+		} catch (PersistenciaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/*
+	public String darDescripcion(String codF, int numR) {
+		String descripcion = null;
+		try {
+			if (ClaseAccesoBD.ExisteFolio(con, codF)) {
+				if (ClaseAccesoBD.ExisteRevision(con, codF, numR)) {
+					descripcion = ClaseAccesoBD.DarDescripcion(con, codF, numR);
+				}
+			}
+		} catch (PersistenciaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return descripcion;
 	}
-
-	public List<VOFolio> listarFolios() throws PersistenciaException, RemoteException {
-		List<VOFolio> ListadoFolios = null;
-		ListadoFolios = Folios.listarFolios();
-		return ListadoFolios;
+	*/
+	public List<VOFolio> listarFolios() {
+		List<VOFolio> Folios = null;
+		try {
+			Folios = daoFolios.listarFolios();
+		} catch (PersistenciaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Folios;
 	}
-
-	public List<VORevision> listarRevisiones(String codF) throws PersistenciaException, RemoteException, LogicaException {
-		List<VORevision> ListadoRevisiones = null;
-		if (Folios.member(codF)) {
-			Folio fol = Folios.find(codF);
-			ListadoRevisiones = fol.listarRevisiones();
-		} else
-			throw new LogicaException("No existe el folio");
-		return ListadoRevisiones;
+	/*
+	public List<VORevision> listarRevisiones (String codF)
+	{
+	
 	}
-
-	public VOFolioMaxRev folioMasRevisado() throws PersistenciaException, RemoteException {
-		VOFolioMaxRev maxFolio = null;
-		maxFolio = Folios.folioMasRevisado();
-		return maxFolio;
+	*/
+	public VOFolioMaxRev folioMasRevisado ()
+	{
+		VOFolioMaxRev voFolMaxRev = null;
+		try {
+			daoFolios.folioMasRevisado();
+		} catch (PersistenciaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return voFolMaxRev;
 	}
 
 }
